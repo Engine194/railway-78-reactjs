664e3d1e62ce2da89d5ca2b970ebb3865ec46e4a
Nguyen Ngoc <ngoc.n@shopstack.asia>
2024-01-22 21:53:45 +0700

    two-way-binding demo

--- hex-rgb-converter/src/App.js
+++ hex-rgb-converter/src/App.js
@@ -1,23 +1,92 @@
+import { useState } from "react";
 import "./App.css";
 import ArrowLeft from "./ArrowLeft";
 import ArrowRight from "./ArrowRight";
+import ColorInput from "./ColorInput";
+import ActionButton from "./ActionButton";
+import { COLOR_TYPE, generateColorInputProps } from "./utils";
 
 function App() {
+  const [hex, setHex] = useState("");
+  const [rgb, setRgb] = useState("");
+
+  const toHex = () => {
+    const numbers = rgb.split(",").map((num) => (+num).toString(16));
+    const newVal = numbers.reduce((acc, val) => {
+      const convertedVal = `${val}`.padStart(2, "0");
+      return `${acc}${convertedVal}`;
+    }, "");
+    setHex(newVal);
+  };
+
+  const toRgb = () => {
+    if (hex.length === 6) {
+      const numbers = hex.split("").reduce((acc, char, i) => {
+        if (i % 2 === 1) {
+          acc[acc.length - 1] += char;
+        } else {
+          acc.push(char);
+        }
+        return acc;
+      }, []);
+      const convertedNumbers = numbers.map((num) => Number.parseInt(num, 16));
+      const convertedRgb = convertedNumbers.join(", ");
+      setRgb(convertedRgb);
+    }
+  };
+
+  const colorPropsMap = new Map([
+    [
+      COLOR_TYPE.HEX,
+      generateColorInputProps(
+        COLOR_TYPE.HEX,
+        "Hex color",
+        "hex-input",
+        hex,
+        setHex
+      ),
+    ],
+    [
+      COLOR_TYPE.RGB,
+      generateColorInputProps(
+        COLOR_TYPE.RGB,
+        "RGB color",
+        "rgb-input",
+        rgb,
+        setRgb
+      ),
+    ],
+  ]);
+
+  const hexInputProps = colorPropsMap.get(COLOR_TYPE.HEX);
+  const rgbInputProps = colorPropsMap.get(COLOR_TYPE.RGB);
+
   return (
     <div className="App">
       <h1 className="greeting-heading">Welcome to Hex-RGB Converter</h1>
-      <div className="demo-color" />
+      <div
+        className="demo-color"
+        style={{
+          backgroundColor: hex ? `#${hex}` : rgb ? `rgb(${rgb})` : "aliceblue",
+        }}
+      />
       <div className="color-form">
-        <input className="color-input hex-input" name="hex" />
+        <ColorInput {...hexInputProps} />
         <div className="btn-actions">
-          <button className="tohex-btn">
-            <ArrowLeft height="24px" width="24px" />
-          </button>
-          <button className="torgb-btn">
-            <ArrowRight height="24px" width="24px" />
-          </button>
+          <ActionButton
+            className="tohex-btn"
+            title="Convert to Hex color"
+            onClick={toHex}
+            Icon={ArrowLeft}
+          />
+          <ActionButton
+            className="torgb-btn"
+            title="Convert to RGB color"
+            onClick={toRgb}
+            Icon={ArrowRight}
+          />
         </div>
-        <input className="color-input rgb-input" name="rgb" />
+        <ColorInput {...rgbInputProps} />
       </div>
     </div>
   );
---
+++ hex-rgb-converter/src/ColorInput.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { COLOR_TYPE } from "./utils";
+
+export default function ColorInput(props) { // UI component
+  const { label, color, changeColor, type, inputClassName } = props || {};
+
+  const handleChangeColor = (event) => {
+    const newVal = event.target.value;
+    changeColor(newVal);
+  };
+
+  return (
+    <fieldset className="input-wrapper">
+      <label htmlFor="hex">{label}:</label>
+      <input
+        id="hex"
+        className={`${inputClassName} color-input`}
+        name="hex"
+        value={color}
+        onChange={handleChangeColor}
+        maxLength={type === COLOR_TYPE.HEX ? 6 : 13}
+      />
+    </fieldset>
+  );
+}
