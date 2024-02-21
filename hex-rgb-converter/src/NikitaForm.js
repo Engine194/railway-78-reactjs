1ef6f34f34525eb95c9c378051fd496c60fe75d8
Nguyen Ngoc <ngoc.n@shopstack.asia>
2024-02-21 20:34:21 +0700

    Commit 50% buoi 21.02

--- hex-rgb-converter/src/NikitaForm.js
+++ hex-rgb-converter/src/NikitaForm.js
@@ -1,18 +1,34 @@
 import { useState } from "react";
 import "./NikitaForm.style.css";
+import { GENDER_TYPE } from "./utils";
 
 const NikitaForm = () => {
-  const [fullName, setFullName] = useState("Nikita194");
-  
-  const handleChangeFullName = (event) => {
+  const [gender, setGender] = useState(GENDER_TYPE.FEMALE);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const onChangeHandlerFactory = (setState) => {
+    return (event) => {
       const inputEle = event.target;
-      setFullName(inputEle.value);
+      setState(inputEle.value);
+    };
+  };
+  const handleChangeGender = onChangeHandlerFactory(setGender);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
   };
 
   return (
     <div className="nikita-form-container">
       <h1>Register new user</h1>
-      <form className="form-row">
+      <form onSubmit={handleSubmit} className="form-row">
         <div className="form-col form-col-left">
           <h2>User information</h2>
           <div className="sub-form">
@@ -22,35 +38,64 @@ const NikitaForm = () => {
                 name="fullname"
                 placeholder="Fullname"
                 value={fullName}
-                onChange={handleChangeFullName}
+                onChange={onChangeHandlerFactory(setFullName)}
               />
               <p>Error fullname</p>
             </div>
 
             <div className="form-control">
-              <input type="text" name="email" placeholder="Email" />
+              <input
+                type="text"
+                value={email}
+                onChange={onChangeHandlerFactory(setEmail)}
+                name="email"
+                placeholder="Email"
+              />
             </div>
 
             <div className="form-control">
-              <input type="text" name="phone" placeholder="Phone number" />
+              <input
+                type="text"
+                value={phoneNumber}
+                onChange={onChangeHandlerFactory(setPhoneNumber)}
+                name="phone"
+                placeholder="Phone number"
+              />
             </div>
 
             <div className="gender-container">
               <span>Gender:</span>
               <div className="gender-radios">
                 <label>
-                  <input type="radio" name="gender" />
+                  <input
+                    onChange={handleChangeGender}
+                    checked={gender === GENDER_TYPE.MALE}
+                    type="radio"
+                    name="gender"
+                    value={GENDER_TYPE.MALE}
+                  />
                   Male
                 </label>
                 <label>
-                  <input type="radio" name="gender" />
+                  <input
+                    onChange={handleChangeGender}
+                    checked={gender === GENDER_TYPE.FEMALE}
+                    type="radio"
+                    name="gender"
+                    value={GENDER_TYPE.FEMALE}
+                  />
                   Female
                 </label>
               </div>
             </div>
 
             <div className="form-control">
-              <input type="date" name="dateOfBirth" />
+              <input
+                type="date"
+                value={dateOfBirth}
+                onChange={onChangeHandlerFactory(setDateOfBirth)}
+                name="dateOfBirth"
+              />
             </div>
           </div>
         </div>
@@ -59,14 +104,28 @@ const NikitaForm = () => {
           <h2>Authentication</h2>
           <div className="sub-form">
             <div className="form-control">
-              <input type="text" name="username" placeholder="Username" />
-            </div>
-            <div className="form-control">
-              <input type="password" name="password" placeholder="Password" />
+              <input
+                type="text"
+                value={username}
+                onChange={onChangeHandlerFactory(setUsername)}
+                name="username"
+                placeholder="Username"
+              />
             </div>
             <div className="form-control">
               <input
                 type="password"
+                value={password}
+                onChange={onChangeHandlerFactory(setPassword)}
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={onChangeHandlerFactory(setConfirmPassword)}
                 name="confirmPassword"
                 placeholder="Confirm password"
               />
