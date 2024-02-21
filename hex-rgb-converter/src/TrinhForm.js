a6f3219c04846dd4befcf756e080b5ba45fd11f9
trinhlee297 <137048872+trinhlee297@users.noreply.github.com>
2024-02-21 18:51:17 +0700

    [update] Exercise

--- hex-rgb-converter/src/TrinhForm.js
+++ hex-rgb-converter/src/TrinhForm.js
@@ -1,176 +1,128 @@
-import './TrinhForm.style.css'
-import {useState} from 'react'
-
-
+import "./TrinhForm.style.css";
+import { useState } from "react";
 
 const TrinhForm = () => {
-    const[fullname, setFullName] = useState('')
-    const handleChangeFullName = (event) =>{
-        const inputEle = event.target;
-        setFullName(inputEle.value);
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [date, setDate] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleChangeFactory = (setState) => {
+    const handleChange = (event) => {
+      const newVal = event.target.value;
+      setState(newVal);
     };
+    return handleChange;
+  };
+  return (
+    <div className="trinh-form-container">
+      <h1>Register Form</h1>
+      <form className="form-row">
+        <div className="form-col form-col-left">
+          <h2>User information</h2>
+          <div className="sub-form">
+            <div className="form-control">
+              <input
+                type="text"
+                name="fullname"
+                placeholder="Full Name"
+                value={fullname}
+                onChange={handleChangeFactory(setFullName)}
+              />
+              {/* <p>Error fullname</p> */}
+            </div>
 
-    console.log('fullname....', fullname);
+            <div className="form-control">
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleChangeFactory(setEmail)}
+              />
+            </div>
 
-    
-    const[email, setEmail] = useState('')
-    const handleChangeEmail = (event) =>{
-        const inputEle = event.target;
-        setEmail(inputEle.value);
-    };
+            <div className="form-control">
+              <input
+                type="text"
+                name="phone"
+                placeholder="Phone Number"
+                value={phone}
+                onChange={handleChangeFactory(setPhone)}
+              />
+            </div>
 
-    // console.log('email....', email);
+            <div className="gender-container">
+              <span>Gender:</span>
+              <div className="gender-radios">
+                <label>
+                  <input type="radio" name="gender" />
+                  Male
+                </label>
+                <label>
+                  <input type="radio" name="gender" />
+                  FeMale
+                </label>
+              </div>
+            </div>
 
-
-    const[phone, setPhone] = useState('')
-    const handleChangePhone = (event) =>{
-        const inputEle = event.target;
-        setPhone(inputEle.value);
-    };
-
-    // console.log('Phone number....',phone);
-
-
-    const[date, setDate] = useState('')
-    const handleChangeDate = (event) =>{
-        const inputEle = event.target;
-        setDate(inputEle.value);
-    };
-
-    // console.log('Date....', date);
-
-
-    const[username, setUserName] = useState('')
-    const handleChangeUserName = (event) =>{
-        const inputEle = event.target;
-        setUserName(inputEle.value);
-    };
-
-    // console.log('user name....', username);
-
-
-    const[password, setPassword] = useState('')
-    const handleChangePassword = (event) =>{
-        const inputEle = event.target;
-        setPassword(inputEle.value);
-    };
-
-    // console.log('password....', password);
-
-
-    const[confirmPassword, setConfirmPassword] = useState('')
-    const handleChangeConfirmPassword = (event) =>{
-        const inputEle = event.target;
-        setConfirmPassword(inputEle.value);
-    };
-
-    // console.log('Confirm password....', confirmPassword);
-
-
-
-
-    return (
-        <div className="trinh-form-container">
-            <h1>Register Form</h1>
-            <form className="form-row">
-                <div className="form-col form-col-left">
-                    <h2>User information</h2>
-                    <div className="sub-form">
-                        <div className="form-control">
-                            <input 
-                            type="text" 
-                            name="fullname" 
-                            placeholder='Full Name'
-                            value={fullname}
-                            onChange={handleChangeFullName}/>
-                            {/* <p>Error fullname</p> */}
-                        </div>
-            
-                        <div className="form-control">
-                            <input 
-                            type="text" 
-                            name="email" 
-                            placeholder='Email'
-                            value={email}
-                            onChange={handleChangeEmail}/>
-                            {/* <p>Error fullname</p> */}
-                        </div>
-
-                        <div className="form-control">
-                            <input
-                            type="text" 
-                            name="phone"
-                            placeholder='Phone Number'
-                            value={phone}
-                            onChange={handleChangePhone}/>
-                            {/* <p>Error fullname</p> */}
-                        </div>
-
-                        <div className="gender-container">
-                            <span>Gender:</span>
-                            <div className="gender-radios">
-                                <label>
-                                    <input type="radio" name="gender"/>
-                                    Male
-                                </label>
-                                <label>
-                                    <input type="radio" name="gender"/>
-                                    FeMale
-                                </label>
-                            </div>
-                        </div>
-
-                        <div className="form-control">
-                            <input
-                            type="date" 
-                            name="dateOfBirth"
-                            value={date}
-                            onChange={handleChangeDate}/>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="diviver"></div>
-
-                <div className="form-col form-col-right">
-                    <h2>Authentication</h2>
-                    <div className="sub-form">
-                        <div className="form-control">
-                            <input type="text" 
-                            name="username" 
-                            placeholder='User Name'
-                            value={username}
-                            onChange={handleChangeUserName}/>
-                        </div>
-
-                        <div className="form-control">
-                            <input type="password" 
-                            name="password" 
-                            placeholder='Password'
-                            value={password}
-                            onChange={handleChangePassword}/>
-                        </div>
-
-                        <div className="form-control">
-                            <input type="password" 
-                            name="confirmPassword" 
-                            placeholder='Confirm Password'
-                            value={confirmPassword}
-                            onChange={handleChangeConfirmPassword}/>
-                        </div>
-                    </div>
-                    <div className='form-actions'>
-                        <button type='reset'>Reset</button>
-                        <button type='submit'>Submit</button>
-                    </div>
-
-                 </div>
-
-            </form>
-
+            <div className="form-control">
+              <input
+                type="date"
+                name="dateOfBirth"
+                value={date}
+                onChange={handleChangeFactory(setDate)}
+              />
+            </div>
+          </div>
         </div>
-    );
 
+        <div className="diviver"></div>
+
+        <div className="form-col form-col-right">
+          <h2>Authentication</h2>
+          <div className="sub-form">
+            <div className="form-control">
+              <input
+                type="text"
+                name="username"
+                placeholder="User Name"
+                value={username}
+                onChange={handleChangeFactory(setUserName)}
+              />
+            </div>
+
+            <div className="form-control">
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChangeFactory(setPassword)}
+              />
+            </div>
+
+            <div className="form-control">
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={handleChangeFactory(setConfirmPassword)}
+              />
+            </div>
+          </div>
+          <div className="form-actions">
+            <button type="reset">Reset</button>
+            <button type="submit">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default TrinhForm;
