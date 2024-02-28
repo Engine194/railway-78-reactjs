8716bd09541a174b3fce27f8685f18419a88b23a
ducchien24081998 <ducchien24081998@gmail.com>
2024-02-28 19:22:06 +0700

    a

--- chienvu-context/src/App.js
+++ chienvu-context/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
-import ComponentA from './ComponentA';
+import { createContext, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ComponentA from "./ComponentA";
 
+export const CountContext = createContext();
+const CountProvider = CountContext.Provider;
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <ComponentA/>
+    <div className="App">
+      <CountProvider
+        value={{
+          count,
+          increaseCountByOne: () => setCount((prev) => prev + 1),
+        }}
+      >
+        <ComponentA />
+      </CountProvider>
+    </div>
   );
 }
 
