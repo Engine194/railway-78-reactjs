4635b1d48a79d06f44b16a9416976ea2aec430e2
trinhlee297 <137048872+trinhlee297@users.noreply.github.com>
2024-02-28 19:19:22 +0700

    update

--- nikita-context/src/App.js
+++ nikita-context/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import ComponentA from './ComponentA';
 
+// export const ComponentContext = createContext();
+// const ComponentProvider = ComponentContext.Provider;
+
 function App() {
   return (
     <div className="App">
