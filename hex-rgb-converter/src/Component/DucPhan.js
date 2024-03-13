cca338782218ac69afcce0d8c57ca6269b087ea7
Nguyen Ngoc <ngoc.n@shopstack.asia>
2024-03-13 20:55:11 +0700

    chua bai ex1 4

--- hex-rgb-converter/src/Component/DucPhan.js
+++ hex-rgb-converter/src/Component/DucPhan.js
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./styles/duc.css";
 import { Link } from "react-router-dom";
-import TextInput from './TextInput';
-import ActionButton from './ActionButton';
-
+import TextInput from "./TextInput";
+import ActionButton from "./ActionButton";
 
 const DucPhan = () => {
-    const [rightInput, setRightInput] = useState("");
-    const [leftInput, setLeftInput] = useState("");
+  const [rightInput, setRightInput] = useState("");
+  const [leftInput, setLeftInput] = useState("");
 
+  const changeLeft = (e) => setLeftInput(e.target.value);
+  const changeRight = (e) => setRightInput(e.target.value);
 
-    return (
-        <div className='container'>
-            <div className='left-input'>
-                <TextInput />
-            </div>
-            <div className='container-btn'>
-                <button className='left-btn'>To right</button>
-                <button className='right-btn'>To left</button>
-            </div>
-            <div className='right-input'>
-                <TextInput />
-            </div>
-        </div>
-    );
+  const toLeft = () => {
+    setRightInput(leftInput);
+    setLeftInput("");
+  };
+
+  const toRight = () => {
+    setLeftInput(rightInput);
+    setRightInput("");
+  };
+
+  return (
+    <div className="container">
+      <div className="left-input">
+        {/* <TextInput /> */}
+        <input value={leftInput} onChange={changeLeft} />
+      </div>
+      <div className="container-btn">
+        <button onClick={toRight} className="left-btn">
+          To right
+        </button>
+        <button onClick={toLeft} className="right-btn">
+          To left
+        </button>
+      </div>
+      <div className="right-input">
+        {/* <TextInput /> */}
+        <input value={rightInput} onChange={changeRight} />
+      </div>
+    </div>
+  );
 };
 
 export default DucPhan;
